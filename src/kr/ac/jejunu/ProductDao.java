9eeef5d6bc33e662debd5ce88bf537f13017140d
leeikjeun <dldlrwjs12@gmail.com>
2017-04-21 17:32:25 +0900

    add tag_12

--- src/kr/ac/jejunu/ProductDao.java
+++ src/kr/ac/jejunu/ProductDao.java
@@ -6,29 +6,49 @@ import java.sql.*;
 public class ProductDao {
 
     private Context context;
-    
+
     public void setContext(Context context) {
         this.context = context;
     }
 
     public Product get(Long id) throws ClassNotFoundException, SQLException {
-        StatementMaker statementMaker = new GetStatementMaker(id);
+        StatementMaker statementMaker = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+        };
         return context.contextGetProduct(statementMaker);
     }
 
     public void add(Product product) throws ClassNotFoundException, SQLException {
-        StatementMaker statementMaker = new AddStatementMaker(product);
+        StatementMaker statementMaker = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT into product(id,title,price) VALUES (?,?,?)");
+            preparedStatement.setLong(1, product.getId());
+            preparedStatement.setString(2,product.getTitle());
+            preparedStatement.setInt(3,product.getPrice());
+            return preparedStatement;
+        };
         context.contextUpdate(statementMaker);
     }
 
     public void update(Product product) throws SQLException {
-        StatementMaker statementMaker = new UpdateStatementMaker(product);
+        StatementMaker statementMaker = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("update product set title = ?, price = ? where id = ?");
+            preparedStatement.setString(1, product.getTitle());
+            preparedStatement.setInt(2,product.getPrice());
+            preparedStatement.setLong(3,product.getId());
+            return preparedStatement;
+        };
         context.contextUpdate(statementMaker);
     }
 
 
     public void delete(Long id) throws SQLException {
-        StatementMaker statementMaker = new DeleteStatementMaker(id);
+        StatementMaker statementMaker = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM product where id = ?");
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+        };
         context.contextUpdate(statementMaker);
     }
 }
